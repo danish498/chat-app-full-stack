0ac229dddf87887496214eef473891a3d725d202
danish498 <daanish629@gmail.com>
2026-03-31 17:04:37 +0530

    clean up

--- back-end/src/utils/bcrypt.util.js
+++
@@ -1,5 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-exports.compareHash = (plainPassword, hashedPassword) => bcrypt.compare(plainPassword, hashedPassword);
-
-exports.createHash = (plainPassword) => bcrypt.hash(plainPassword, 10);
